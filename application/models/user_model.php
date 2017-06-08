815d9339228dcf34c2f7c19812bfafdc1117d67f
mac <2675601413@qq.com>
2017-06-08 08:29:14 +0800

    //chen

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -2,11 +2,13 @@
 
 class User_model extends CI_Model
 {
+    //chen
     public function check_login($tel,$password){
         $sql = "select * from t_user where tel='$tel' and password='$password' ";
         $query = $this->db->query($sql);
         return $query->row();
     }
+    //chen
     public function select_user_by_id($id){
         $sql="select * from t_user where user_id='$id'";
         $query=$this->db->query($sql);
